fb0e169db2d510efbe9c4c548a85fa8c079e1a4e
HuuThang <thangmt1997@gmail.com>
2018-03-31 21:26:09 +0700

    update form main

--- QLKhoHang/QLKhoHang/GUI/frmMain.cs
+++ QLKhoHang/QLKhoHang/GUI/frmMain.cs
@@ -44,26 +44,24 @@ namespace QLKhoHang
 
         private void thêmKháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            frmQLkh kh = new frmQLkh();
-            kh.Show();
+            GUI.ThemDoiTac dt = new GUI.ThemDoiTac();
+            dt.Show();
         }
 
         private void thêmĐốiTácToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            frmQLnhacc nhacc = new frmQLnhacc();
-            nhacc.Show();
+            GUI.ThemDoiTac dt = new GUI.ThemDoiTac();
+            dt.Show();
         }
 
         private void thêmHóaĐơnNhậpToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            frmQLhoadonnhap hdnhap = new frmQLhoadonnhap();
-            hdnhap.Show();
+
         }
 
         private void thêmHóaĐơnXuấtToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            frmQLhoadonxuat hdxuat = new frmQLhoadonxuat();
-            hdxuat.Show();
+
         }
 
         private void danhSáchNhàCungCấpToolStripMenuItem_Click(object sender, EventArgs e)
